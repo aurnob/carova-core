87801c0322e48d0afcc5f93cfd2662af1f37584f
Aurnob Salam <aurnob.salam@gmail.com>
2026-03-03 22:21:52 +0600

    modified medusa config file

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,10 +5,15 @@ loadEnv(process.env.NODE_ENV || "development", process.cwd());
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
+    workerMode: process.env.WORKER_MODE as "shared" | "worker" | "server",
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      storeCors:
+        process.env.STORE_CORS ||
+        "http://localhost:8000,https://localhost:3000",
+      adminCors:
+        process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001",
+      authCors: process.env.AUTH_CORS || "http://localhost:9000",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
@@ -17,5 +22,27 @@ module.exports = defineConfig({
     // Remove 'title' from here!
     // Only use properties like 'disable' or 'path' if needed.
     // path: `/app/admin`,
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    backendUrl: process.env.ADMIN_BACKEND_URL || "http://localhost:9000",
   },
+  modules: [
+    {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+  ],
 });
